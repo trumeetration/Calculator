ee2b19683d216b4769f34d53ea58f7e04f11ded4
Pavel <war.naruto@mail.ru>
2020-11-30 01:23:26 +0500

    Released Interface IMemory

---
+++ Interfaces/IMemory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Interfaces
+{
+    interface IMemory
+    {
+        ObservableCollection<string> Memory { get; }
+
+        void Add(string value);
+        void Remove(int index);
+        void Increase(int index, string value);
+        void Decrease(int index, string value);
+        void Clear();
+    }
+}
---
+++ Models/MemoryRAM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.Interfaces;
+
+namespace Calculator.Models
+{
+    public class MemoryRAM : IMemory
+    {
+        public MemoryRAM()
+        {
+            Memory = Memory ?? new ObservableCollection<string>();
+        }
+        public ObservableCollection<string> Memory { get; }
+
+        public void Add(string value)
+        {
+            Memory.Add(value);
+        }
+
+        public void Remove(int index)
+        {
+            Memory.RemoveAt(index);
+        }
+
+        public void Increase(int index, string value)
+        {
+            Memory[index] = Convert.ToString(Convert.ToDouble(value) + Convert.ToDouble(Memory[index]));
+        }
+
+        public void Decrease(int index, string value)
+        {
+            Memory[index] = Convert.ToString(Convert.ToDouble(value) - Convert.ToDouble(Memory[index]));
+        }
+
+        public void Clear()
+        {
+            Memory.Clear();
+        }
+    }
+}
